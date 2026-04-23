f388418777d4003c377ee25205036d31494d565d
Gadgetsmela <chandnikumari9386@gmail.com>
2026-04-23 07:42:37 +0530

    Delete SignupClient.tsx

--- SignupClient.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-
-export default function SignupClient() {
-  const searchParams = useSearchParams();
-
-  return <div>Signup Page</div>;
-}
